bbacfd37f135838686e95c68d78cff627687dc4d
Eugene Yu <eugeneyu@Eugenes-iMac.local>
2019-01-28 22:14:30 -0800

    create theme joy

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -729,8 +729,8 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * example.org, with all subdomains included.
  */
 $settings['trusted_host_patterns'] = array(
-  '^localhost$',
-  '^.+\.localhost$',
+  '^ccifellowship$',
+  '^ccifellowship\.local$',
 );
 /**
  * The default list of directories that will be ignored by Drupal's file API.
