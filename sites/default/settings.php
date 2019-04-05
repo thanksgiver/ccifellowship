7a7af52495b7fb370c50bb683ff118ea3d6852c7
Eugene Yu <coolbee.china@gmail.com>
2019-04-05 13:46:32 -0700

    20190405

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -730,7 +730,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  */
 $settings['trusted_host_patterns'] = array(
   '^ccifellowship$',
-  '^ccifellowship\.local$',
+  '^ccifellowship\.wci$'
 );
 /**
  * The default list of directories that will be ignored by Drupal's file API.
